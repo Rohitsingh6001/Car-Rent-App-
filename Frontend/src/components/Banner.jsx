bf0461abc2382abb0672c7f47c5ca58444bb230a
Rohitsingh6001 <Rohitsingh837000@gmail.com>
2025-07-23 07:23:41 +0530

    Ok completed

---
+++ Frontend/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='flex flex-col md:flex-row md:items-start items-center justify-between px-8 min-md:pl-14 pt-10 bg-gradient-to-r from-[#0558fe] to-[#a9cfff] max-w-6xl mx-3 md:mx-auto rounded-2xl overflow-hidden'>
+
+     
+     
+    </div>
+  )
+}
+
+export default Banner
