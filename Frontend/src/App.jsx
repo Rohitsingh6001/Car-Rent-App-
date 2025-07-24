c69a21aa3ffe23b671d19c40545f89c0b9b30c0a
Rohitsingh6001 <Rohitsingh837000@gmail.com>
2025-07-24 12:01:41 +0530

    Ok completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,6 +6,11 @@ import Home from './pages/Home';
 import CarDetails from './pages/CarDetails';
 import Cars from './pages/Cars';
 import MyBookings from './pages/MyBookings';
+import Layout from './pages/owner/Layout.jsx'
+import AddCar from './pages/owner/AddCar.jsx';
+import Dashboard from './pages/owner/Dashboard.jsx';
+import ManageBookings from './pages/owner/ManageBookings.jsx';
+import ManageCars from './pages/owner/ManageCar.jsx';
 
 const App = () => {
 
@@ -22,9 +27,14 @@ const App = () => {
         <Route path='/car-details/:id' element={<CarDetails/>}/>
         <Route path='/cars' element={<Cars/>}/>
         <Route path='/my-bookings' element={<MyBookings/>}/>
-
+        <Route path='/owner' element={<Layout/>}>
+          <Route index element={<Dashboard/>}/>
+          <Route path='add-car' element={<AddCar/>}/>
+          <Route path='manage-cars' element={<ManageCars/>}/>
+          <Route path='manage-bookings' element={<ManageBookings/>}/>
+        </Route>
       </Routes>
-      <Footer/>
+      
 
     </>
   )
